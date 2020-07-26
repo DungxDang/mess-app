c937909a5632d620d107819d2417ec5627fed393
kai <dung.ddang@gmail.com>
2020-07-26 17:21:32 +0700

    start1

--- mess-app/src/App.js
+++ mess-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ChatRoom from './chat-room';
 
 function App() {
   return (
     <div className="App">
-     
+      <ChatRoom/>
+      <p>hfhffh</p>
     </div>
   );
 }
---
+++ mess-app/src/chat-room.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ChatRoom() {
+  return (
+    <div style={{width:"50%"}}>
+      <div > User1
+      </div>
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default ChatRoom;
